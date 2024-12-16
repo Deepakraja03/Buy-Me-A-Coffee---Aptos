0633f19944f99fe759b98ffe9f0fdc4ccb290f32
Deepakraja03 <12306deepakraja@gmail.com>
2024-12-16 07:25:41 +0530

    added balance

--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -1,15 +1,17 @@
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createEntry as createEntryFunction } from "@/entry-functions/integrateContract";
 import { aptosClient } from "../utils/aptosClient";
 import { useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { fetchBalance } from "@/utils/getFunction";
 
 
 function Home() {
   const [name, setName] = useState('');
   const [message, setMessage] = useState('');
   const [value, setValue] = useState('');
+  const [balance, setBalance] = useState<any>();
   const { account, signAndSubmitTransaction } = useWallet();
   const queryClient = useQueryClient();
 
@@ -45,6 +47,26 @@ function Home() {
     }
   }
 
+  const viewBalance = async() => {
+    try {
+      const walletAddress = String(account?.address);
+      const balance = await fetchBalance(walletAddress);
+      if (!balance) {
+        console.warn("Balance fetch returned null or undefined");
+        return;
+      }
+      const formattedBalance = balance.balance/100000000 || 0;
+      setBalance(formattedBalance);
+      return balance;
+    } catch (error) {
+      console.error("Error fetching balance", error);
+    }
+  }
+  useEffect(() => {
+    viewBalance();
+    console.log("logging aptos balance", balance);
+  }, [account?.address]);
+
   return (
     <>
       <div className="min-h-screen bg-gradient-to-r from-purple-300 via-pink-300 to-yellow-300 flex justify-center">
@@ -72,14 +94,25 @@ function Home() {
               value={value}
               onChange={(e) => setValue(e.target.value)}
             />
-            <button
-              className={`w-full mt-6 px-4 py-2 text-white font-semibold rounded-lg transition-all 
-                          ${!name || !message || !value ? "bg-gray-400 cursor-not-allowed" : "bg-purple-600 hover:bg-purple-700"}`}
-              onClick={buycoffee}
-              disabled={!name || !message || !value}
-            >
-              Buy Coffee
-            </button>
+            {balance === 0 || balance < value ? (
+                <button
+                className={`w-full mt-6 px-4 py-2 text-white font-semibold rounded-lg transition-all 
+                            ${ balance === 0 || balance < value   ? "bg-gray-400 cursor-not-allowed" : "bg-purple-600 hover:bg-purple-700"}`}
+                onClick={buycoffee}
+                disabled={balance === 0 || balance < value}
+              >
+                Not Enough Balance
+              </button>
+            ) : (
+                <button
+                className={`w-full mt-6 px-4 py-2 text-white font-semibold rounded-lg transition-all 
+                            ${!name || !message || !value ? "bg-gray-400 cursor-not-allowed" : "bg-purple-600 hover:bg-purple-700"}`}
+                onClick={buycoffee}
+                disabled={!name || !message || !value}
+              >
+                Buy Coffee
+              </button>
+            )} 
           </div>
         </div>
       </div>
--- frontend/utils/getFunction.ts
+++ frontend/utils/getFunction.ts
@@ -1,5 +1,9 @@
 import { APTOS_MOVE_MODULE_ADDR } from "@/constants";
 import { AptosClient } from "aptos";
+import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+ 
+const config = new AptosConfig({ network: Network.DEVNET });
+const aptos = new Aptos(config);
 
 const client = new AptosClient("https://fullnode.devnet.aptoslabs.com");
 
@@ -25,6 +29,24 @@ export const fetchViewFunctionByAddress = async (walletAddress : String) => {
       arguments: [walletAddress],
     });
     return result;
+  } catch (error) {
+    console.error("Error calling view function:", error);
+    return null;
+  }
+};
+
+export const fetchBalance = async (walletAddress : string) => {
+  try {
+    const coinType = "0x1::aptos_coin::AptosCoin";
+    const [balanceStr] = await aptos.view<[string]>({
+      payload: {
+        function: "0x1::coin::balance",
+        typeArguments: [coinType],
+        functionArguments: [walletAddress]
+      }
+    });
+    const balance = parseInt(balanceStr, 10);
+    return {balance: balance, balanceStr: balanceStr};
   } catch (error) {
     console.error("Error calling view function:", error);
     return null;
